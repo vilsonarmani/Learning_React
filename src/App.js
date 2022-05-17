4e234e42d285a6b2046d70e972e41a9059c3b78a
Vilson Pereira <Vilson.Pereira@ao3tech.com>
2022-05-16 22:28:48 -0300

    StateLesson - State with arrays and objects

--- src/App.js
+++ src/App.js
@@ -1,101 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import {Component} from 'react';
 
-class App extends Component {
-
-  /* 
-   instead of using the class constructor, you can use the class fields
-   */
-  /*
-  constructor(props) {
-    super(props);
-    // boundFunction ou Bound Method
-    //this.handlePClick = this.handlePClick.bind(this);
-    // may you have to use arrow functions to bind the context
-    // like this: this.handlePClick = () => this.handlePClick();
-
-    this.state = {
-      name: 'Luciana',
-      age: '28',
-      counter: 0
-    };
-
-  }*/
+class App extends  Component{
 
   state = {
-    name: 'Luciana',
-    age: '28',
-    counter: 0
+    posts: [
+      {
+        id: 1,
+        title: 'Titulo 1',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+      },
+      {
+        id: 2,
+        title: 'Titulo 2',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem 2.'
+      },
+      {
+        id: 3,
+        title: 'Titulo 3',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem 3.'
+      },
+    ]
   };
 
-  handlePClick = () => {
-    this.setState({ name: 'Vilson' });
-    /* const { name } = this.state;
-    console.log(`<p> Clicado ${name}`); */
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault(); //previne o comportamento padrão do evento
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  }
-
-
-  render() {
-    const { name, age, counter } = this.state;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            Click <code>{name} - {age} anos</code>.
-          </p>
-          <a
-            className="App-link"
-            onClick={this.handleAClick}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link Clicado {counter} vezes
-          </a>
-        </header>
-      </div>
-    );
-  }
+  render(){
+  const { posts } = this.state;
+  return (
+    <div className="App">
+      {posts.map(post => (
+        <div> 
+          <h1 key={post.id}>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+        )  
+      )}
+    </div>
+  );
 }
-
-
-// function App(luciana) {
-//   let valor;
-
-//   if (luciana.hasvalue)
-//     valor = luciana
-//   else
-//     valor = 'src/App.js';
-
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           <br />
-//           Edit <code>{valor}</code> e salve para recarregar. <br />
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
+}
 export default App;
